0b1007784c56d6879244f93c4af3fb9468a03154
M-asaki-K <47930865+M-asaki-K@users.noreply.github.com>
2024-05-02 16:17:16 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from ClassAssignment import ClassAssignment
+
+if __name__ == "__main__":
+    classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']  # クラスリスト
+    assignment = ClassAssignment('students.csv', 'student_pairs.csv', classes)
+    assignment.apply_all_constraints()  # すべての制約を自動で適用
+
+    status = assignment.solve()
+    print(f'Status: {status}')
+    assignment.plot_results()
